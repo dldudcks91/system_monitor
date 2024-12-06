543d1f3dd242b87d777794e872680a3b31343a8c
LYC <dldudcks91@gmail.com>
2024-12-06 19:18:45 +0900

    20241206

--- system_monitor.py
+++ system_monitor.py
@@ -1,39 +1,23 @@
 #%%
 import os
-import asyncio
-import logging
 import psutil
 import json
 import time
 from datetime import datetime
 
-from logging.handlers import TimedRotatingFileHandler
-#import websockets
+
+import redis
 
 
-class SystemMonitor:
-    def __init__(self, websocket_port=8765):
+class AsyncSystemMonitor:
+    def __init__(self):
         
         self.log_dir="/var/log/system_monitor"
         os.makedirs(self.log_dir, exist_ok=True)
         
-        self.port = websocket_port
-        self.clients = set()
-        self.logger = self._setup_logger()
+        self.redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
         
-    def _setup_logger(self):
-        logger = logging.getLogger('system_monitor')
-        logger.setLevel(logging.INFO)
-        
-        handler = TimedRotatingFileHandler(
-            'system_metrics.log',
-            when='midnight',
-            interval=1,
-            backupCount=7  # 일주일치만 보관
-        )
-        handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
-        logger.addHandler(handler)
-        return logger
+   
         
     def get_metrics(self):
         
@@ -64,19 +48,33 @@ class SystemMonitor:
                 pass
         
         # CPU 사용률 기준으로 정렬하고 상위 limit개 선택
-        processes = sorted(processes, key=lambda x: x['cpu_percent'], reverse=True)
+        processes_cpu = sorted(processes, key=lambda x: x['cpu_percent'], reverse=True)[:limit]
+        processes_memory = sorted(processes, key=lambda x: x['memory_percent'], reverse=True)[:limit]
+        processes_total = {**processes_cpu, **processes_memory}
         
         
-        return processes
+        return processes_total
     
     
-    def test_monitor(self):
+    def store_metrics(self, metrics):
+        
+        self.redis_client.set('current_system_monitor', json.dumps(metrics))
+        # 히스토리 저장 (리스트 사용)
+        self.redis_client.lpush('system_monitor_history', json.dumps(metrics))
+        # 최대 1000개만 유지
+        self.redis_client.ltrim('system_monitor_history', 0, 999)
+        
+        
+    
+    def monitor(self):
         last_save = 0
         metrics_buffer = []
         while True:
             try:
                 metrics = self.get_metrics()
+                self.store_metrics(metrics)
                 current_time = datetime.now().timestamp()
+                
                 if current_time - last_save >= 60:
                     date_str = datetime.now().strftime('%Y-%m-%d')
                     json_file = os.path.join(self.log_dir, f"system_monitor_{date_str.replace('-','')}.json") 
@@ -89,54 +87,17 @@ class SystemMonitor:
                     
                     metrics_buffer = []
                     last_save = current_time
-                    print(f'Save metrics: {current_time}')
+                    #print(f'Save metrics: {current_time}')
                     
                     
-                print(metrics)
+                #print(metrics)
                 metrics_buffer.append(metrics)
                 time.sleep(3)
             except Exception as e:
                 print(f"Error in test_monitor: {e}")
                 time.sleep(3)
         
-    async def monitor_metrics(self):
-        last_save = 0
-        
-        while True:
-            try:
-                metrics = self.get_metrics()
-                current_time = datetime.now().timestamp()
-                
-                # 웹소켓 브로드캐스트 (3초마다)
-                if self.clients:  # 클라이언트가 있을 때만 전송
-                    message = json.dumps(metrics)
-                    await asyncio.gather(
-                        *[client.send(message) for client in self.clients]
-                    )
-                
-                # 파일 저장 (60초마다)
-                if current_time - last_save >= 60:
-                    self.logger.info(json.dumps(metrics))
-                    last_save = current_time
-                    
-                await asyncio.sleep(3)
-                
-            except Exception as e:
-                print(f"Error in monitor_metrics: {e}")
-                await asyncio.sleep(3)
-                
-    async def handle_client(self, websocket):
-        self.clients.add(websocket)
-        try:
-            await websocket.wait_closed()
-        finally:
-            self.clients.remove(websocket)
-            
-    async def start(self):
-        async with websockets.serve(self.handle_client, "localhost", self.port):
-            await self.monitor_metrics()
-
+   
 if __name__ == "__main__":
-    monitor = SystemMonitor()
-    #asyncio.run(monitor.start())
-    monitor.test_monitor()
+    monitor = AsyncSystemMonitor()
+    monitor.monitor()
